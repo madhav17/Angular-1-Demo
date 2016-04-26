1ecd33e6a183022edbd7cf11ce91b40f7d9489de
Madhav <madhav.khanna@tothenew.com>
2016-04-27 01:29:40 +0530

    L5 completed

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
-// in globlal namespace
-var person="Madhav";
+var Person = function(firstName,lastName){
+    this.firstName = firstName;
+    this.lastName = lastName;
+}
 
-objectApp.logPerson();
+function logPerson(john){
+    // it dependency 
+    // so we use dependency injection
+//    var john = new Person("John","Doe");
+    console.log(john);
+}
+
+var john = new Person("John","Doe");
+// now this dependency injection becoz we are passing it and it is not dependent
+logPerson(john);
