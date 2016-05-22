55a3ef84efa246021bab4e0df54511ce371d9967
Madhav <madhav.khanna@tothenew.com>
2016-05-22 16:29:50 +0530

    L8 completed

--- app.js
+++ app.js
@@ -2,17 +2,13 @@ var myApp = angular.module('myApp',[]);
 
 myApp.controller('mainController', function($scope){
 
+    console.log($scope);
 });
 
-var searchPeople = function (firstName,lastName,height,age,occupation){
+var searchPeople = function (firstName,$scope,height,age,occupation){
     
     
     return "Madhav"
 }
 
-console.log(searchPeople());
-console.log(searchPeople(1,2,3,4));
-console.log(searchPeople(1,2,3,4,5));
-console.log(searchPeople);
-var searchPeopleString = searchPeople.toString();
-console.log(searchPeopleString);
+console.log(angular.injector().annotate(searchPeople));
